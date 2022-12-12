19e2102ea0c8098f4895f8fd28e5f307f09e3bd1
okore431 <olussskin@gmail.com>
2022-12-12 01:07:38 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["chubby", "funny"];
+  let noun = ["alpaca", "unicorn"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+};
